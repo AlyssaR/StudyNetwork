3995ff47d17cf0f89b4c26555b34f86a2dc7a22f
quincyschurr <qschurr@smu.edu>
2015-03-19 22:10:06 +0000

    trying to add data to profile

--- bin/createProfile2.php
+++ bin/createProfile2.php
@@ -16,10 +16,12 @@ p {
 </style>
 <body>
 
+
 <?php
 $database = new mysqli("localhost", "web", "wearegeniuses", "StudyNetwork");
-if ($database->connect_errno)
+if (!$database)
     die("Connection failed: " . $database->connect_error);
+
 
 	$fName = $POST['f_name'];
 	$lName = $POST['l_name'];
@@ -27,12 +29,9 @@ if ($database->connect_errno)
 	$password = $_POST['passwd'];
 	$uid = $_POST['uid'];
 
-	//$dataInserted = $database->query("SELECT * FROM Users");
 	$sqlRes = $database->query("SELECT * FROM Users");
-	//$result = mysql_query($sqlRes)
 
 	if($sqlRes === NULL)
-		//$response = array("success"=>false, "id"=>0, )
 		echo "There is no data";
 	else
 	{
@@ -42,8 +41,6 @@ if ($database->connect_errno)
 			echo "id: " . $row["uid"]. " -- Name: " . $row["f_name"]. " " . $row["l_name"]. " --Email: " . $row["email"]."
 			<br>";
 		}
-		//$response = array("success"=>true, "f_name"=>$sqlRes['f_name'], "l_name"=>$sqlRes['l_name'], "uid"=>$sqlRes['uid'], "email"=>$sqlRes['email'], "password"=>$sqlRes['passwd']);
-		//print_r ($response)
 	}
 
 	if($fName === "" || $lName === "" || $email === "" || $password === "" || uid === ""){
@@ -51,28 +48,37 @@ if ($database->connect_errno)
 		
 	}
 	else{
-		$duplicateCheck = $database->query("SELECT email FROM Users WHERE email = '$email' LIMIT 1");
+		$duplicateCheck = $database->query("SELECT * FROM Users WHERE (uid = '$uid') AND (email = '$email') LIMIT 1");
+		$num_rows = mysql_fetch_array($duplicateCheck);
+		if($num_rows > 0)
+		{
+			echo "User Already Exists";
+		}
+		else
+		{
+			$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($newID, $fName, $lName, $email, $password");
+		}
+
+		/*$duplicateCheck = $database->query("SELECT email, uid FROM Users WHERE email = '$email' AND uid = '$uid' LIMIT 1");
 		$checkResults = $duplicateCheck->fetch_assoc();
 		if(!($checkResults === NULL))
 			echo "You are the only user with this email";
 		else
 		{
-			$prevUser = $database->query("SELECT uid FROM Users ORDER BY uid DESC LIMIT 1");
-			$row = $prevUser->fetch_assoc();
-			if($row === NULL){
-				$outputJSON = array ('uid' => $uid);
-				$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($uid, $fName, $lName, $email, $password)");
+			//$prevUser = $database->query("SELECT uid FROM Users ORDER BY uid DESC LIMIT 1");
+			//$row = $prevUser->fetch_assoc();
+			//if($row === NULL){
+				//$outputJSON = array ('uid' => $uid);
+				//$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($uid, $fName, $lName, $email, $password)");
 			}
 			else{
 				$newID = $row['uid']+1;
 				$outputJSON = array ('uid'=>$newID);
 				$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($newID, $fName, $lName, $email, $password");
 			}
-		}
+		}*/
 	}
 ?>
-
-
 	<h2><font color = rgb(51,102,153)> Please enter the following information </font></h2>
 	<form method="post" action="createProfile2.php" id = "createAccount" name = "createAccount">
 	First Name: <input type = "text" name = "f_name" /><br />
