f012d3a8d7f7ee2fb2d49b51dc0838c6fde94351
vagrant <vagrant@precise64.(none)>
2015-03-19 03:18:37 +0000

    Fixed syntax error in db pop script

--- database/populateScript.sql
+++ database/populateScript.sql
@@ -2,4 +2,4 @@ USE 'StudyNetwork'
 
 insert into Users values (1, 'Alex', 'Bay', 'abay@smu.edu', 'abcdef56'),
 	(2, 'Bob', 'Carter', 'bcarter@smu.edu', 'blehblah'), (3, 'Connor', 'Dean', 'cdean@smu.edu', 'seeher'),
-	(4, 'Dana', 'Eggett', 'danae@smu.edu', 'somepass'), (5 'Emily', 'Gahill', 'egahill@smu.edu', 'weeee');
+	(4, 'Dana', 'Eggett', 'danae@smu.edu', 'somepass'), (5, 'Emily', 'Gahill', 'egahill@smu.edu', 'weeee');
