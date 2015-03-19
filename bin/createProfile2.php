b10b614ef7219fc7086f09935257ec621ca9b461
quincyschurr <qschurr@smu.edu>
2015-03-19 19:20:14 +0000

    got createProfile2.php to mostly work

--- bin/createProfile2.php
+++ bin/createProfile2.php
@@ -17,50 +17,44 @@ p {
 <body>
 
 <?php
+$database = new mysqli("localhost", "web", "wearegeniuses", "StudyNetwork");
+if ($database->connect_errno)
+    die("Connection failed: " . $database->connect_error);
 
-$app->post('/createUserAccount', function () {
-	global $mysqli;
 	$fName = $POST['f_name'];
 	$lName = $POST['l_name'];
 	$email = $_POST['email'];
 	$password = $_POST['passwd'];
 	$uid = $_POST['uid'];
-	if($fName === "" || $lName === "" || $email === "" || $password === "" || uid === "")
-		$outputJSON = array ('uid'=>-2);
+	if($fName === "" || $lName === "" || $email === "" || $password === "" || uid === ""){
+		echo "Please enter data in all of the fields";
+		
+	}
 	else{
-		$duplicateCheck = $mysqli->query("SELECT email FROM Users WHERE email = '$email' LIMIT 1");
+		$duplicateCheck = $database->query("SELECT email FROM Users WHERE email = '$email' LIMIT 1");
 		$checkResults = $duplicateCheck->fetch_assoc();
 		if(!($checkResults === NULL))
 			$outputJSON = array ('uid'=>-1);
 		else
 		{
-			$prevUser = $mysqli->query("SELECT uid FROM Users ORDER BY uid DESC LIMIT 1");
+			$prevUser = $database->query("SELECT uid FROM Users ORDER BY uid DESC LIMIT 1");
 			$row = $prevUser->fetch_assoc();
 			if($row === NULL){
 				$outputJSON = array ('uid' => $uid);
-				$insertion = $mysqli->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($uid, $fName, $lName, $email, $password)");
+				$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($uid, $fName, $lName, $email, $password)");
 			}
 			else{
 				$newID = $row['uid']+1;
 				$outputJSON = array ('uid'=>$newID);
-				$insertion = $mysqli->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($newID, $fName, $lName, $email, $password");
+				$insertion = $database->query("INSERT INTO Users (uid, f_name, l_name, email, passwd) VALUES ($newID, $fName, $lName, $email, $password");
 			}
 		}
 	}
-});
-
-
-$app->post('/loginUser', function () {
-	$response = array ('status' => "Success", "id"=>$row['uid'], "f_name"=>$row['f_name'], "l_name"=>$row['l_name']);
-	$email = $_POST['email'];
-	$password = $_POST['passwd'];
-	echo json_encode($response);
-});
 ?>
 
 
 	<h2><font color = rgb(51,102,153)> Please enter the following information </font></h2>
-	<form method="post" action="#" id = "createAccount" name = "createAccount">
+	<form method="post" action="createProfile2.php" id = "createAccount" name = "createAccount">
 	First Name: <input type = "text" name = "f_name" /><br />
 	Last Name: <input type = "text" name = "l_name" /><br />
 	SMU ID: <input type = "text" name = "uid" /><br />
