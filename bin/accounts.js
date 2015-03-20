a807a17b756b3b6d99582c8d3f17ceab0a726bfa
AlyssaR <arahman@smu.edu>
2015-03-20 16:18:30 +0000

    Alphabetized functions in accounts.js... No really.

--- bin/accounts.js
+++ bin/accounts.js
@@ -18,63 +18,6 @@ function authenticate() {
     });
 }
 
-function validate() {
-    console.log("You pushed the login button");
-    
-    var regexName = /\w*@smu\.edu/;
-    var regexPass = /[\w!@#$%&*;'"_]{8,64}/;
-
-    var UserName = document.getElementById("email").value;
-    var UserPass = document.getElementById("password").value;
-    
-    var test1 = regexName.test(UserName);
-    var test2 = regexPass.test(UserPass);
-    
-    console.log(test1);
-    console.log(test2);
-    
-    if(test1 && test2) {
-        console.log("PostCall");
-        authenticate();
-    }
-}
-
-function register() {
-    $.ajax({
-        url: "api/register",
-        type: "post",
-        data: {
-            "f_name":$("#f_name").val(), 
-            "l_name":$("#l_name").val(), 
-            "uid":$("#uid").val(), 
-            "email":$("#email").val(), 
-            "passwd":$("#passwd").val()
-        },
-        dataType: "json",
-        success: function(data) {
-            if(data.success) {
-                alert("Welcome, " + data.f_name + "!");
-                window.location = "index.html";
-            }
-            else
-                alert("Error: " + data.errorType);
-        }
-    });
-}
-
-function redirect() {
-    window.location = "register.html";
-}
-
-function signIn(value) {
-    var date = new Date();
-    date.setTime(date.getTime() + (30*60*1000)); //Login expires in 30 minutes
-    var expires = "; expires=" + date.toGMTString();
-
-    document.cookie = "sn_uid=" + value + expires + "; path=/";
-    window.location = "editprofile.html";
-}
-
 function getID() {
     var name = "sn_uid=";
     var ca = document.cookie.split(';');
@@ -104,4 +47,61 @@ function getName() {
 function logout() {
     document.cookie = "sn_uid=;expires=-1;path=/";
     window.location = "index.html";
+}
+
+function redirect() {
+    window.location = "register.html";
+}
+
+function register() {
+    $.ajax({
+        url: "api/register",
+        type: "post",
+        data: {
+            "f_name":$("#f_name").val(), 
+            "l_name":$("#l_name").val(), 
+            "uid":$("#uid").val(), 
+            "email":$("#email").val(), 
+            "passwd":$("#passwd").val()
+        },
+        dataType: "json",
+        success: function(data) {
+            if(data.success) {
+                alert("Welcome, " + data.f_name + "!");
+                window.location = "index.html";
+            }
+            else
+                alert("Error: " + data.errorType);
+        }
+    });
+}
+
+function signIn(value) {
+    var date = new Date();
+    date.setTime(date.getTime() + (30*60*1000)); //Login expires in 30 minutes
+    var expires = "; expires=" + date.toGMTString();
+
+    document.cookie = "sn_uid=" + value + expires + "; path=/";
+    window.location = "editprofile.html";
+}
+
+function validate() {
+    console.log("You pushed the login button");
+    
+    var regexName = /\w*@smu\.edu/;
+    var regexPass = /[\w!@#$%&*;'"_]{8,64}/;
+
+    var UserName = document.getElementById("email").value;
+    var UserPass = document.getElementById("password").value;
+    
+    var test1 = regexName.test(UserName);
+    var test2 = regexPass.test(UserPass);
+    
+    console.log(test1);
+    console.log(test2);
+    
+    if(test1 && test2) {
+        console.log("PostCall");
+        authenticate();
+    }
 }
