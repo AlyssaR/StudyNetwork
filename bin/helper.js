5096f6a8ffe7343d89d1526e9b2e4a1b56e93295
AlyssaR <arahman@smu.edu>
2015-03-20 14:54:14 +0000

    Working on fixing helper.js

--- bin/helper.js
+++ bin/helper.js
@@ -1,3 +1,24 @@
+function createClass() {
+    $.ajax({
+        url: "api/addClass",
+        type: "post",
+        data: {
+            "dept":$("#dept").val(), 
+            "class_num":$("#class_num").val()
+            "time2":$("#time2").val(), 
+            "professor":$("#professor").val(), 
+        },
+        dataType: "json",
+        success: function(data) {
+            if(data.success) {
+                alert("Class added successfully!");
+                window.location = "editprofile.html";
+            }
+            else
+                alert("Error:" + data.errorType);
+        }
+    });
+}
 
 function redirectToClass() {
     window.location = "createClassForm.html";
