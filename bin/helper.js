afb2ec41d1515749d640bfaed1201c363729cf40
quincyschurr <qschurr@smu.edu>
2015-04-21 03:15:33 +0000

    okay groups now show up

--- bin/helper.js
+++ bin/helper.js
@@ -156,6 +156,17 @@ function getGroups() {
     });
 }
 
+/*function getOrganizations() {
+    $.ajax({
+        url: "api/getOrganizations",
+        type: "post",
+        dataType: "json",
+        success: function(data) {
+            var table = document.getElementById('')
+        }
+    })
+}*/
+
 function getGroupsForProfile() {
     $ajax({
         url:"api/GetGroupsRow",
